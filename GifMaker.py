67aebd15553a59c323a099b2fb2535a760b26cfe
VictorLier <victor@lier.dk>
2023-01-09 16:14:39 +0100

    Lidt mere reasonable hastighed på gif

--- GifMaker.py
+++ GifMaker.py
@@ -3,7 +3,7 @@ import imageio.v2 as imageio
 
 png_dir = 'C:\\Users\\Victor\\Documents\\DTU\\41812 FEA\\Project 2\\ANSYS'
 GifName = 'Anim.gif'
-Time = 0.04
+Time = 0.5
 
 images = []
 for file_name in sorted(os.listdir(png_dir)):
