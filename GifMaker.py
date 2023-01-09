14bf9597ecff57febeee3e5e2e4c41482fe28813
VictorLier <victor@lier.dk>
2023-01-09 16:03:32 +0100

    Lidt lækkert Gif ;)

---
+++ GifMaker.py
@@ -0,0 +1,14 @@
+import os
+import imageio.v2 as imageio
+
+png_dir = 'C:\\Users\\Victor\\Documents\\DTU\\41812 FEA\\Project 2\\ANSYS'
+GifName = 'Anim.gif'
+Time = 0.04
+
+images = []
+for file_name in sorted(os.listdir(png_dir)):
+    if file_name.endswith('.PNG'):
+        file_path = os.path.join(png_dir, file_name)
+        images.append(imageio.imread(file_path))
+
+imageio.mimsave(png_dir + '\\' + GifName,images,duration=Time)
