3cbb6fe49a452103ee68187a248fd658fa8b9ae0
VictorLier <victor@lier.dk>
2023-01-10 13:35:20 +0100

    Fixet GifMaker - Sorterer korrekt

--- GifMaker.py
+++ GifMaker.py
@@ -3,12 +3,16 @@ import imageio.v2 as imageio
 
 png_dir = 'C:\\Users\\Victor\\Documents\\DTU\\41812 FEA\\Project 2\\ANSYS'
 GifName = 'Anim.gif'
-Time = 0.5
+Time = 0.1
+
+l = os.listdir(png_dir)
+l = [val for val in l if val.endswith(".PNG")]
 
 images = []
-for file_name in sorted(os.listdir(png_dir)):
+for file_name in sorted(l, key=lambda x: int(x.split(".")[0])) :
     if file_name.endswith('.PNG'):
         file_path = os.path.join(png_dir, file_name)
         images.append(imageio.imread(file_path))
+        print(file_name)
 
 imageio.mimsave(png_dir + '\\' + GifName,images,duration=Time)
